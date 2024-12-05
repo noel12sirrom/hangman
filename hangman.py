780ce922dc09b866b3d03cc16c0c91d5c5abbdf6
Leon <104960228+noel12sirrom@users.noreply.github.com>
2024-12-05 07:44:00 -0500

    "Removed comment characters from dictionary definitions in hangman.py"

--- hangman.py
+++ hangman.py
@@ -5,14 +5,14 @@ trys = 7
 word = ""
 
 mylist = {
-    #"mouse":"A computer input device",
-    #"violin":" A musical stringed instrument",
-    #"storm":"A type of weather phenomenon characterized by heavy rain and strong winds",
-    #"nitros":"A type of chemical compound that contains nitrogen and oxygen atoms and can be directly injected into the air intake to make more POWER",
-    #"eclipse":"A type of astronomical event where the moon passes between the Earth and the sun",
-    #"arthritis":"A type of medical condition characterized by inflammation and pain in the joints",
-    #"nostalgia":"A sentimental longing or wistful affection for the past, typically for a period of time when one was happy",
-    #"promiscuous":"Engaging in sexual relations with many people; having no strong emotional attachment to any one person; willing to have sex with anyone",
+    "mouse":"A computer input device",
+    "violin":" A musical stringed instrument",
+    "storm":"A type of weather phenomenon characterized by heavy rain and strong winds",
+    "nitros":"A type of chemical compound that contains nitrogen and oxygen atoms and can be directly injected into the air intake to make more POWER",
+    "eclipse":"A type of astronomical event where the moon passes between the Earth and the sun",
+    "arthritis":"A type of medical condition characterized by inflammation and pain in the joints",
+    "nostalgia":"A sentimental longing or wistful affection for the past, typically for a period of time when one was happy",
+    "promiscuous":"Engaging in sexual relations with many people; having no strong emotional attachment to any one person; willing to have sex with anyone",
     "pen":"A writing instrument used to apply ink to paper or other surfaces.",
     "chair":"A piece of furniture with a flat surface and four legs, typically used for sitting.",
     "Sunflower":"A tall plant that produces large yellow flowers and is often grown for its edible seeds.",
